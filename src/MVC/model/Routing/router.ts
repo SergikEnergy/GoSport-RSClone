8f12aa1a2accb95ba2107d47c9de3a9e5b60a2e6
VladBoisa <vladboisa@gmail.com>
2023-02-10 20:38:25 +0200

    feat: add events listener after render Main page

--- src/MVC/model/Routing/router.ts
+++ src/MVC/model/Routing/router.ts
@@ -1,5 +1,6 @@
 import iRoute from "./TStype/iRoute";
 import main from '../../view/pages/main/main'
+import { afterRenderMainPage } from "../../view/pages/main/main";
 import notFound from '../../view/pages/notFound/404';
 import prod from '../../view/pages/prod/prod';
 import { renderHeader } from '../../view/header/header';
@@ -25,6 +26,7 @@ if (header != undefined && footer != undefined) {
 //Rendering content
 if (content !=undefined){
   await pageRender(content);
+  await afterRenderMainPage();
 }
 }
 export default router
