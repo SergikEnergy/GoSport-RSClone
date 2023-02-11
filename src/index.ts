fc617b7f6c6efe0856e892920ba722dc62550c38
VladBoisa <vladboisa@gmail.com>
2023-02-11 17:14:09 +0200

    refactor: change name of folder mvc from caps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 import './styles/styles1.scss';
-import router from './MVC/model/Routing/router'
+import router from './mvc/model/Routing/router'
 console.log('start');
 // Listen on hash change:
 window.addEventListener('hashchange',router)
