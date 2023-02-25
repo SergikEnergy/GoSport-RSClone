5776aec5a47d30e9f93c581a51b5c0ce6c417793
VladBoisa <vladboisa@gmail.com>
2023-02-25 12:51:15 +0200

    refactor: separate structure of interface in file

--- src/MVC/view/pages/main/mainQueryDB.ts
+++ src/MVC/view/pages/main/mainQueryDB.ts
@@ -1,25 +1,17 @@
 
+import { iEvents } from "./TStype/mainTypes";
 export const fetchEventByKind = async (url:URL,kind:string) => {
   try {
     const req = await fetch(url);
     const data = await req.json();
-    return filterEventsByKind(data,kind)
+    return await filterEventsByKind(data,kind);
   }
   catch (e){
     console.log(e);
   }
 }
-interface iEvents {
-  kind: string,
-  date?: { type: string},
-  id_place?: { type: string},
-  time_start?: { type: string},
-  time_end?: { type: string},
-  place_name?: { type: string},
 
-
-}
-function filterEventsByKind(eventsArr:Array<iEvents>, search:string):Array<unknown> {
+function filterEventsByKind(eventsArr:Array<iEvents>, search:string):Array<iEvents> {
   return eventsArr
     .filter((values)=>(values.kind === search))
 }
