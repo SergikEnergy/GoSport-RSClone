494cbe4d727bebb0130449cb4633d7dbd49ab06c
VladBoisa <vladboisa@gmail.com>
2023-02-25 12:49:28 +0200

    refactor: change name of types in main file

---
+++ src/MVC/view/pages/main/TStype/mainTypes.ts
@@ -0,0 +1,8 @@
+export interface iEvents {
+  kind: string,
+  date?: { type: string},
+  id_place?: { type: string},
+  time_start?: { type: string},
+  time_end?: { type: string},
+  place_name?: { type: string},
+}
