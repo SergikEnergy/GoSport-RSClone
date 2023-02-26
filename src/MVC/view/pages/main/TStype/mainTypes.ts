5696e8f3553fb8233bd4edd6f3cafa922cb17092
SergEnergy <79093952+SergikEnergy@users.noreply.github.com>
2023-02-26 05:21:32 +0300

    feat: update events types

--- src/MVC/view/pages/main/TStype/mainTypes.ts
+++ src/MVC/view/pages/main/TStype/mainTypes.ts
@@ -1,8 +1,11 @@
 export interface iEvents {
+_id?: string, 
   kind: string,
   date?: { type: string},
   id_place?: { type: string},
   time_start?: { type: string},
   time_end?: { type: string},
   place_name?: { type: string},
+  rest_players?: number,
+  players?: string[]
 }
